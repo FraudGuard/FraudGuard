65c5ae1dda036c9f90c7ebea666cb17e6b1b475a
Christopher Barnes User <christopher@barnes.biz>
2021-04-11 23:13:24 +0200

    add ribbons to ads for probability

--- foreground.js
+++ foreground.js
@@ -1,5 +1,49 @@
-let items = document.getElementsByClassName('itemtile-header');
-let posts = [];
-for (i = 0; i < items.length; i += 1) {
-  posts.push(items[i].firstElementChild.href);
+let itemtiles = document.getElementsByClassName('itemtile-body');
+
+for (let i = 0; i < itemtiles.length; i += 1) {
+  let fraudRibbon = document.createElement('div');
+  fraudRibbon.className = 'fraud-ribbon';
+
+  let fraudProbability = document.createElement('span');
+  fraudProbability.className = 'fraud-probability';
+
+  fraudRibbon.appendChild(fraudProbability);
+  itemtiles[i].appendChild(fraudRibbon);
+}
+
+// Mocking the probability
+let mockData = document.getElementsByClassName('fraud-probability');
+for (let i = 0; i < mockData.length; i += 1) {
+  let ran = Math.round(Math.random() * 100);
+  mockData[i].innerHTML = ran;
+  mockData[i].setAttribute('fraud-probability', ran);
+}
+// if VB oder job kein mock genereiren
+
+const between = (x, min, max) => {
+  return x >= min && x <= max;
+};
+
+let fraudProbabilities = document.getElementsByClassName('fraud-probability');
+
+// TODO DISGUSTING FIX THIS SHIT ASAP
+for (let i = 0; i < fraudProbabilities.length; i += 1) {
+  if (fraudProbabilities[i].innerHTML < 19) {
+    fraudProbabilities[i].parentElement.classList.add('green');
+  }
+  if (fraudProbabilities[i].innerHTML > 60) {
+    fraudProbabilities[i].parentElement.classList.add('red');
+  }
+  if (
+    fraudProbabilities[i].innerHTML < 30 &&
+    fraudProbabilities[i].innerHTML > 19
+  ) {
+    fraudProbabilities[i].parentElement.classList.add('yellow');
+  }
+  if (
+    fraudProbabilities[i].innerHTML < 59 &&
+    fraudProbabilities[i].innerHTML > 30
+  ) {
+    fraudProbabilities[i].parentElement.classList.add('orange');
+  }
 }
