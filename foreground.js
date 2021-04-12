78373edfdfe41fdbf05bb3ef13abb9ffd0138699
Christopher Barnes User <christopher@barnes.biz>
2021-04-12 22:14:53 +0200

    stop flagging non-ads with fraud

--- foreground.js
+++ foreground.js
@@ -1,7 +1,7 @@
 let itemtiles = document.getElementsByClassName('itemtile-body');
 
 // Generating a Ribbon for each tile ==> should be each ads (exclude tiles that are not an ad)
-for (tile of itemtiles) {
+for (let tile of itemtiles) {
   let fraudRibbon = document.createElement('div');
   fraudRibbon.className = 'fraud-ribbon';
 
@@ -14,9 +14,9 @@ for (tile of itemtiles) {
 
 // Mocking the probability
 let mockData = document.getElementsByClassName('fraud-probability');
-for (x of mockData) {
+for (let x of mockData) {
   let ran = Math.round(Math.random() * 100);
-  x.innerHTML = ran;
+  x.innerHTML = 'Scam: ' + ran + '%';
   x.setAttribute('fraud-probability', ran);
 }
 
@@ -24,24 +24,27 @@ for (x of mockData) {
 let fraudProbabilities = document.getElementsByClassName('fraud-probability');
 
 // Assigning background-color class to the ribbons based on the probability in the spans
-for (fraudProbability of fraudProbabilities) {
-  if (fraudProbability.innerHTML < 19) {
-    fraudProbability.parentElement.classList.add('green');
+for (let fraudProbability of fraudProbabilities) {
+  if (fraudProbability.getAttribute('fraud-probability') < 19) {
     fraudProbability.parentElement.classList.add('green');
   } else if (
-    fraudProbability.innerHTML < 30 &&
-    fraudProbability.innerHTML > 19
+    fraudProbability.getAttribute('fraud-probability') < 30 &&
+    fraudProbability.getAttribute('fraud-probability') > 19
   ) {
-    fraudProbability.parentElement.classList.add('yellow');
     fraudProbability.parentElement.classList.add('yellow');
   } else if (
-    fraudProbability.innerHTML < 59 &&
-    fraudProbability.innerHTML > 30
+    fraudProbability.getAttribute('fraud-probability') < 59 &&
+    fraudProbability.getAttribute('fraud-probability') > 30
   ) {
-    fraudProbability.parentElement.classList.add('orange');
     fraudProbability.parentElement.classList.add('orange');
   } else {
     fraudProbability.parentElement.classList.add('red');
-    fraudProbability.parentElement.classList.add('red');
+  }
+}
+
+// Remove probabilities on ads that dont have a price
+for (let item of itemtiles) {
+  if (item.childElementCount === 3) {
+    item.removeChild(item.lastChild);
   }
 }
