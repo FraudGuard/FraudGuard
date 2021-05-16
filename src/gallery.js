58929702c9894a5938ae9e8a1a0c15a19b487d6c
Christopher Barnes User <christopher@barnes.biz>
2021-05-16 14:11:10 +0200

    fix gallery score rendering

--- src/gallery.js
+++ src/gallery.js
@@ -1,19 +1,6 @@
-let itemtiles = document.getElementsByClassName('itemtile-body');
-
-// Generating a Ribbon for each tile ==> should be each ads (exclude tiles that are not an ad)
-for (const tile of itemtiles) {
-  const fraudRibbon = document.createElement('div');
-  fraudRibbon.className = 'fraud-ribbon';
-
-  const fraudProbability = document.createElement('span');
-  fraudProbability.className = 'fraud-probability';
-
-  fraudRibbon.appendChild(fraudProbability);
-  tile.appendChild(fraudRibbon);
-}
+const itemtiles = document.getElementsByClassName('itemtile-body');
 
 // making request to the backend server and attach the score to the fraud-ribbon
-itemtiles = document.getElementsByClassName('itemtile-header');
 for (const tile of itemtiles) {
   const matches = tile.innerHTML.match(/[0-9]{9,}/);
   const id = matches[0];
@@ -21,40 +8,40 @@ for (const tile of itemtiles) {
     fetch(`http://localhost:4200/api/ads/${id}`)
       .then((response) => response.json())
       .then((data) => {
-        tile.nextSibling.childNodes[3].firstChild.innerHTML = `Scam: ${data.ad.fraud_score}%`;
-        tile.nextSibling.childNodes[3].firstChild.setAttribute(
-          'fraud-probability',
-          data.ad.fraud_score,
-        );
+        if (data.ad !== undefined) {
+          const fraudRibbon = document.createElement('div');
+          fraudRibbon.className = 'fraud-ribbon';
+
+          const fraudProbability = document.createElement('span');
+          fraudProbability.className = 'fraud-probability';
+
+          fraudProbability.innerHTML = `Scam: ${data.ad.fraud_score}%`;
+          fraudProbability.setAttribute(
+            'fraud-probability',
+            data.ad.fraud_score,
+          );
+
+          const fraudScore = fraudProbability.getAttribute('fraud-probability');
+          if (fraudScore < 19) {
+            fraudProbability.parentElement.classList.add('green');
+          } else if (fraudScore < 30 && fraudScore > 19) {
+            fraudProbability.parentElement.classList.add('yellow');
+          } else if (fraudScore < 59 && fraudScore > 30) {
+            fraudProbability.parentElement.classList.add('orange');
+          } else {
+            fraudProbability.parentElement.classList.add('red');
+          }
+
+          fraudRibbon.appendChild(fraudProbability);
+          tile.appendChild(fraudRibbon);
+        }
       });
   }
 }
 
-// Grabbing all the spans with fraud-probability
-const fraudProbabilities = document.getElementsByClassName('fraud-probability');
-
-// Assigning background-color class to the ribbons based on the probability in the spans
-for (const fraudProbability of fraudProbabilities) {
-  if (fraudProbability.getAttribute('fraud-probability') < 19) {
-    fraudProbability.parentElement.classList.add('green');
-  } else if (
-    fraudProbability.getAttribute('fraud-probability') < 30 &&
-    fraudProbability.getAttribute('fraud-probability') > 19
-  ) {
-    fraudProbability.parentElement.classList.add('yellow');
-  } else if (
-    fraudProbability.getAttribute('fraud-probability') < 59 &&
-    fraudProbability.getAttribute('fraud-probability') > 30
-  ) {
-    fraudProbability.parentElement.classList.add('orange');
-  } else {
-    fraudProbability.parentElement.classList.add('red');
-  }
-}
-
 // Remove probabilities on ads that dont have a price
-for (const item of itemtiles) {
-  if (item.childElementCount === 3) {
-    item.removeChild(item.lastChild);
+for (const tile of itemtiles) {
+  if (tile.childElementCount === 3) {
+    tile.removeChild(tile.lastChild);
   }
 }
