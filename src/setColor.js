81b75bd431fc52386108adbdaedf507784df2edf
Christopher Barnes User <christopher@barnes.biz>
2021-05-18 16:02:16 +0200

    change ranking values

--- src/setColor.js
+++ src/setColor.js
@@ -8,14 +8,14 @@ const wahrscheinlichkeiten = {
 /* eslint-disable no-param-reassign */
 const setColor = (score, mount, style) => {
   if (style === 'button') {
-    if (score < -50) {
+    if (score < -30) {
       mount.style.backgroundColor = 'green';
       mount.innerHTML = `${wahrscheinlichkeiten[1]}`;
-    } else if (score > -50 && score < 0) {
+    } else if (score > -30 && score < 10) {
       mount.style.backgroundColor = 'yellow';
       mount.style.color = 'black';
       mount.innerHTML = `${wahrscheinlichkeiten[2]}`;
-    } else if (score > 0 && score < 50) {
+    } else if (score > 10 && score < 40) {
       mount.style.backgroundColor = 'orange';
       mount.innerHTML = `${wahrscheinlichkeiten[3]}`;
     } else {
@@ -23,13 +23,13 @@ const setColor = (score, mount, style) => {
       mount.innerHTML = `${wahrscheinlichkeiten[4]}`;
     }
   } else if (style === 'tag') {
-    if (score < -50) {
+    if (score < -30) {
       mount.classList.add('green-tag');
       mount.innerHTML = `${wahrscheinlichkeiten[1]}`;
-    } else if (score > -50 && score < 0) {
+    } else if (score > -30 && score < 10) {
       mount.classList.add('yellow-tag');
       mount.innerHTML = `${wahrscheinlichkeiten[2]}`;
-    } else if (score > 0 && score < 50) {
+    } else if (score > 10 && score < 40) {
       mount.classList.add('orange-tag');
       mount.innerHTML = `${wahrscheinlichkeiten[3]}`;
     } else {
@@ -37,13 +37,13 @@ const setColor = (score, mount, style) => {
       mount.innerHTML = `${wahrscheinlichkeiten[4]}`;
     }
   } else if (style === 'gallery') {
-    if (score < -50) {
+    if (score < -30) {
       mount.parentElement.classList.add('green');
       mount.innerHTML = `${wahrscheinlichkeiten[1]}`;
-    } else if (score > -50 && score < 0) {
+    } else if (score > -30 && score < 10) {
       mount.parentElement.classList.add('yellow');
       mount.innerHTML = `${wahrscheinlichkeiten[2]}`;
-    } else if (score > 0 && score < 50) {
+    } else if (score > 10 && score < 40) {
       mount.parentElement.classList.add('orange');
       mount.innerHTML = `${wahrscheinlichkeiten[3]}`;
     } else {
