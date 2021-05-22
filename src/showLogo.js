150a545ad2f1fffef0c735e6d004c9338527729b
Christopher Barnes User <christopher@barnes.biz>
2021-05-22 19:50:15 +0200

    Aktivierung der Browser Extension sichtbar machen

---
+++ src/showLogo.js
@@ -0,0 +1,15 @@
+const link = document.createElement('a');
+const logo = document.createElement('img');
+
+link.href = 'https://github.com/FraudGuard/FraudGuard';
+
+logo.src =
+  'https://avatars.githubusercontent.com/u/68067715?s=40&u=33674b40df5283e3db2731cb4f14531cc8217690&v=4';
+
+const ebay = document.getElementById('site-logo');
+
+ebay.style.display = 'flex';
+ebay.style.flexDirection = 'row';
+
+link.appendChild(logo);
+ebay.appendChild(link);
