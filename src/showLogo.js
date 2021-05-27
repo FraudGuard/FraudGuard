0957e73d55b670e2e9bd9ea88d26aae96ae3551f
Christopher Barnes User <christopher@barnes.biz>
2021-05-27 21:35:45 +0200

    fix logo positioning

--- src/showLogo.js
+++ src/showLogo.js
@@ -11,10 +11,9 @@ link.href = 'https://github.com/FraudGuard/FraudGuard';
 logo.src =
   'https://avatars.githubusercontent.com/u/68067715?s=40&u=33674b40df5283e3db2731cb4f14531cc8217690&v=4';
 
-const ebay = document.getElementById('site-logo');
+const ebay = document.getElementsByClassName('logo')[0];
 
-ebay.style.display = 'flex';
-ebay.style.flexDirection = 'row';
+ebay.style.marginLeft = '250px';
 
 link.appendChild(logo);
 ebay.appendChild(link);
