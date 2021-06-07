d849eaf863b6ef43419dbe35ef8fb7e71047854d
Tim Strakeljahn <timstrakeljahn@live.de>
2021-06-07 16:52:32 +0200

    logo position

--- src/showLogo.js
+++ src/showLogo.js
@@ -13,10 +13,15 @@ logo.src = 'https://avatars.githubusercontent.com/u/83354594?s=200&v=4';
 logo.width = '100';
 logo.height = '100';
 
+// document.getElementById('site-logo').style.cssText += 'display:flex;align-items:center'
+document.getElementById('site-logo').style.cssText += 'position:relative'
 const ebay = document.getElementById('site-logo');
 
-logo.style.margin = '0px 0px 50px 350px';
-logo.style.position = 'absolute';
+// logo.style.margin = '0px 0px 50px 350px';
+link.style.position = 'absolute';
+link.style.left = '50%';
+link.style.top = '-20px';
+link.style.transform = 'translateX(-50%)';
 
 link.appendChild(logo);
-ebay.insertBefore(link, ebay.childNodes[0]);
+ebay.append(link, ebay.childNodes[0]);
